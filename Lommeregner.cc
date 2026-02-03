ade680ea59d7db60c8eb181829faba0a6eb73b83
mostermand <mostermand@gmail.com>
2026-02-03 11:57:58 +0100

    Minimal viable calculator
    You can add, subtract, multiply and divide pairs of numbers.
    
    More complicated expressions are still not possible

--- Lommeregner.cc
+++ Lommeregner.cc
@@ -43,13 +43,42 @@ enum class Token {
                 //Empty loop body, j is length of num; Shortcircuit means we never access the right side of && if it would overflow
                 for(; i+j < rawText.size() && ('0' <= rawText[i+j] && rawText[i+j] <= '9'); j++) {}
                 tokenOut.push_back(std::make_pair(Token::Num,rawText.substr(i,j)));
-                i = i+j;
+                //after parsing number increase i to index of last digit of number
+                i = i+j - 1;
             } else {
                 //does not recognize the symbol.
             }
             break;
         }
     }
+ }
+
+ 
+ int eval(std::vector<std::pair<Token, std::string> > tokenList) {
+    int result = 0;
+    if(tokenList.size() == 3) {
+        switch(tokenList[1].first)
+        {
+        case Token::Plus:
+            return std::stoi(tokenList[0].second) + std::stoi(tokenList[2].second);
+        case Token::Minus:
+            return std::stoi(tokenList[0].second) - std::stoi(tokenList[2].second);
+        case Token::Prod:
+            return std::stoi(tokenList[0].second) * std::stoi(tokenList[2].second);
+        case Token::Div:
+            if(std::stoi(tokenList[2].second) == 0) {
+                std::cout<<"Division med 0\n";
+            } else {
+                return std::stoi(tokenList[0].second) / std::stoi(tokenList[2].second);
+            }
+            
+        
+        default:
+            break;
+        }
+    }
+    std::cout<<"Brug <num1>[+-*/]<num2>";
+    return -1;
  }
 
 int main(int argc, char* argv[]) {
@@ -61,6 +90,9 @@ int main(int argc, char* argv[]) {
         std::cout<<">";
         std::getline(std::cin, input);
         lexString(input, tokens);
+        std::cout<<eval(tokens)<<std::endl;
+
+        tokens.clear();
     }
     
 }
