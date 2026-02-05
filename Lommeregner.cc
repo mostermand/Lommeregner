c265b44b69fb29345bf5501513a57c1273f3cddf
mostermand <mostermand@gmail.com>
2026-02-05 15:58:41 +0100

    Remove whitespace

--- Lommeregner.cc
+++ Lommeregner.cc
@@ -3,33 +3,33 @@
 #include <string>
 
 #include "Lexing.h"
- 
- int eval(TokenVector tokenList) {
-    int result = 0;
-    if(tokenList.size() == 3) {
-        switch(tokenList[1].first)
-        {
-        case TokenId::Plus:
-            return std::stoi(tokenList[0].second) + std::stoi(tokenList[2].second);
-        case TokenId::Minus:
-            return std::stoi(tokenList[0].second) - std::stoi(tokenList[2].second);
-        case TokenId::Prod:
-            return std::stoi(tokenList[0].second) * std::stoi(tokenList[2].second);
-        case TokenId::Div:
-            if(std::stoi(tokenList[2].second) == 0) {
-                std::cout<<"Division med 0\n";
-            } else {
-                return std::stoi(tokenList[0].second) / std::stoi(tokenList[2].second);
-            }
-            
-        
-        default:
-            break;
-        }
-    }
-    std::cout<<"Brug <num1>[+-*/]<num2>";
-    return -1;
- }
+
+int eval(TokenVector tokenList) {
+   int result = 0;
+   if(tokenList.size() == 3) {
+       switch(tokenList[1].first)
+       {
+       case TokenId::Plus:
+           return std::stoi(tokenList[0].second) + std::stoi(tokenList[2].second);
+       case TokenId::Minus:
+           return std::stoi(tokenList[0].second) - std::stoi(tokenList[2].second);
+       case TokenId::Prod:
+           return std::stoi(tokenList[0].second) * std::stoi(tokenList[2].second);
+       case TokenId::Div:
+           if(std::stoi(tokenList[2].second) == 0) {
+               std::cout<<"Division med 0\n";
+           } else {
+               return std::stoi(tokenList[0].second) / std::stoi(tokenList[2].second);
+           }
+           
+       
+       default:
+           break;
+       }
+   }
+   std::cout<<"Brug <num1>[+-*/]<num2>";
+   return -1;
+}
 
 int main(int argc, char* argv[]) {
     std::string input = "";
